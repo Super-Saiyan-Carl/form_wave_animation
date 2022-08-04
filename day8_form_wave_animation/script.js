25c772f475e34e2bdc725881dacf67cad8108764
Super Saiyan Carl <101432134+Super-Saiyan-Carl@users.noreply.github.com>
2022-08-04 06:51:06 -0600

    completed
    open with live server

--- day8_form_wave_animation/script.js
+++ day8_form_wave_animation/script.js
@@ -1,3 +1,8 @@
 const labels = document.querySelectorAll('.form-control label')
 
-label
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 25}ms">${letter}</span>`)
+        .join('')
+})
